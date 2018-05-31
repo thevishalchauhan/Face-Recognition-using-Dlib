a44f708aa51d40dfc9d9ed606858e20bf6713780
Vishal Chauhan <vishal0143@users.noreply.github.com>
2018-06-01 01:22:31 +0530

    Add files via upload

---
+++ face_recognition/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+__author__ = """Vishal Chauhan"""
+__email__ = 'vishal.chauhan314236@gmail.com'
+__version__ = '1.1'
+
+from .api import load_image_file, face_locations, batch_face_locations, face_landmarks, face_encodings, compare_faces, face_distance
